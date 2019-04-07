25ee8c401645214132ed845edca5421dd4e44a08
medestin <laskawski.marcin@gmail.com>
2019-04-07 12:59:30 +0200

    feat: expand Converter.convert to be more abstract

--- src/main/java/com/medestin/converter/Converter.java
+++ src/main/java/com/medestin/converter/Converter.java
@@ -2,23 +2,48 @@ package com.medestin.converter;
 
 import com.medestin.interpreter.InputInterpreter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Converter implements Conversion{
     private final InputInterpreter interpreter;
+    private final String BASE = "g";
+    private final Map<String, Double> units = new HashMap<>();
 
     public Converter() {
         this.interpreter = new InputInterpreter();
+        units.put(BASE, 1.0);
+        units.put("kg", 1000.0);
+        units.put("lb", 453.592);
     }
 
     public double convert(String input){
         ConverterDTO convertedInput = interpreter.interpret(input);
-        return convertedInput.getValue()/2.205;
+        if(!isConversionPossible(convertedInput.getFrom(), convertedInput.getTo())){
+            throw new RuntimeException("Unsupported conversion unit(s)");
+        }
+
+        if(convertedInput.getFrom().equals(convertedInput.getTo())){
+            return convertedInput.getValue();
+        }
+
+        double valueBase = convertToBase(convertedInput.getValue(), convertedInput.getFrom());
+        return convertFromBase(valueBase, convertedInput.getTo());
     }
 
     private double convertToBase(double value, String from){
-        return 0.0;
+        return isBase(from) ? value : value*units.get(from);
     }
 
     private double convertFromBase(double value, String to){
-        return 0.0;
+        return isBase(to) ? value : value/units.get(to);
+    }
+
+    private boolean isBase(String input){
+        return input.equals(BASE);
+    }
+
+    private boolean isConversionPossible(String from, String to){
+        return units.containsKey(from) && units.containsKey(to);
     }
 }
