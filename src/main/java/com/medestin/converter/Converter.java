2c960807598bd659d7bd8a6c0a5ae352ac41a749
medestin <laskawski.marcin@gmail.com>
2019-04-07 11:39:54 +0200

    refactor Converter.convert to be more abstract

--- src/main/java/com/medestin/converter/Converter.java
+++ src/main/java/com/medestin/converter/Converter.java
@@ -3,6 +3,10 @@ package com.medestin.converter;
 public class Converter {
 
     public double convert(String input){
-        return 1.814;
+        String[] arrayInput = input.split("[ ]");
+        double value = Double.parseDouble(arrayInput[0]);
+        String unitFrom = arrayInput[1];
+        String unitTo = arrayInput[2];
+        return value/2.205;
     }
 }
